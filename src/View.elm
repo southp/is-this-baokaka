2ab3aa029a9196c7271cad069d9e8e022ac22d32
James Tien <southp0105@gmail.com>
2017-03-28 23:49:26 +0800

    Add some basic styles.

--- src/View.elm
+++ src/View.elm
@@ -14,9 +14,20 @@ import Model exposing ( .. )
 
 view : AppState -> Html Msg
 view { queryString, queryStatus } =
-    div []
+    div
+    [ class "isthisbaokaka__wrapper" ]
     [
-        h2 [] [ text "這是寶卡卡嗎？" ],
-        input [ type_ "text", placeholder "輸入建商大名", onInput UpdateQueryString ] [],
-        button [ onClick SubmitQuery ] [ text "好緊張喔..." ]
+        h2 [ class "isthisbaokaka__heading-text" ] [ text "這是寶卡卡嗎？" ],
+
+        input
+        [ class "isthisbaokaka__input-text-field"
+        , type_ "text"
+        , placeholder "請輸入建商大名..."
+        , onInput UpdateQueryString
+        ] [],
+
+        button
+        [ class "isthisbaokaka__main-button"
+        , onClick SubmitQuery
+        ] [ text "好緊張喔..." ]
     ]
