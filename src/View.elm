9592989f82b3ce208a2b245361d1fdd1ef06e7fc
James Tien <southp0105@gmail.com>
2017-03-30 14:38:37 +0800

    Wrap the input fields in a proper <form> element.

--- src/View.elm
+++ src/View.elm
@@ -20,17 +20,24 @@ view ( { queryString, queryResult } as state ) =
     [
         h2 [ class "isthisbaokaka__heading-text" ] [ text "這是寶卡卡嗎？" ],
 
-        input
-        [ class "isthisbaokaka__input-text-field"
-        , type_ "text"
-        , placeholder "請輸入建商大名..."
-        , onInput UpdateQueryString
-        ] [],
+        Html.form
+        [ action "#"
+        , onSubmit SubmitQuery ]
+        [
+            input
+            [ class "isthisbaokaka__input-text-field"
+            , type_ "text"
+            , placeholder "請輸入建商大名..."
+            , autofocus True
+            , onInput UpdateQueryString
+            ] [],
 
-        button
-        [ class "isthisbaokaka__main-button"
-        , onClick SubmitQuery
-        ] [ text "好緊張喔..." ],
+            button
+            [ class "isthisbaokaka__main-button"
+            , onClick SubmitQuery
+            , type_ "submit"
+            ] [ text "好緊張喔..." ]
+        ],
 
         resultSection state
     ]
