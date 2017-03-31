a65a0c031cf4006ed35860577b02d54b005bd2e3
James Tien <southp0105@gmail.com>
2017-03-31 13:59:36 +0800

    typo: exlude => exclude

--- src/Update.elm
+++ src/Update.elm
@@ -38,7 +38,7 @@ update msg state =
             Cmd.none
         )
 
--- exlude the post ID 92 since it is the announcement post.
+-- exclude the post ID 92 since it is the announcement post.
 postApi : String -> String
 postApi query = "https://public-api.wordpress.com/rest/v1.1/sites/isthisbaokaka.wordpress.com/posts?fields=title&exclude=92&search=" ++ query
 
